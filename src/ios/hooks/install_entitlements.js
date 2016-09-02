78ce42e0a5eb566472aaae8b5e871c081faed00a
Hernan Zhou <hernanzh@gmail.com>
2016-09-02 13:44:30 +0200

    typo

--- src/ios/hooks/install_entitlements.js
+++ src/ios/hooks/install_entitlements.js
@@ -6,7 +6,7 @@ var console_log = function (txt) {
 var writeLog = function (iosFolder, projName) {
   var fs = require('fs');
   var dest = path.join(iosFolder, projName, 'www', 'cordova_log.txt');
-  fs.writeFile(dest, logStr, function(err) {
+  fs.writeFile(dest, logString, function(err) {
       if(err) {
           return console.log(err);
       }
