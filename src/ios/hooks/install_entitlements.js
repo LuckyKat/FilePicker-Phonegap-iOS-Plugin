49353ece7102e38ba07471d4ea440058d8b4f08d
Hernan Zhou <hernanzh@gmail.com>
2016-09-02 14:28:34 +0200

    more robust logging

--- src/ios/hooks/install_entitlements.js
+++ src/ios/hooks/install_entitlements.js
@@ -1,16 +1,35 @@
 var logString = "";
-var console_log = function (txt) {
-  logString += txt + '\n';
-  console.error(txt);
+var hasInit = false;
+var initLog = function (iosFolder) {
+    var dest = path.join(iosFolder, 'www', 'cordova_log.txt');
+    var logStream = fs.createWriteStream(dest, {
+        'flags': 'a'
+    });
+    hasInit = true;
+    // use {'flags': 'a'} to append and {'flags': 'w'} to erase and write a new file
+    // logStream.write('install entitlements');
+
 };
-var writeLog = function (iosFolder, projName) {
-  var fs = require('fs');
-  var dest = path.join(iosFolder, 'www', 'cordova_log.txt');
-  fs.writeFile(dest, logString, function(err) {
-      if(err) {
-          return console.log(err);
+var console_log = function (txt) {
+    if (hasInit) {
+      if (logString) {
+        logStream.write(logString);
+        logString = '';  
       }
-  }); 
+      logStream.write(text);  
+    } else {
+      logString += txt + '\n';
+    }
+    console.error(txt);
+};
+var writeLog = function (iosFolder) {
+    // var fs = require('fs');
+    // fs.writeFile(dest, logString, function (err) {
+    //     if (err) {
+    //         return console.log(err);
+    //     }
+    // });
+    logStream.end('end');
 };
 
 // using error to see if this shows up in AB
@@ -20,79 +39,81 @@ var xcode = require('xcode'),
     fs = require('fs'),
     path = require('path');
 
-  module.exports = function (context) {
-  var Q = context.requireCordovaModule('q');
-  var deferral = new Q.defer();
+module.exports = function (context) {
+    var Q = context.requireCordovaModule('q');
+    var deferral = new Q.defer();
 
-  if (context.opts.cordova.platforms.indexOf('ios') < 0) {
-    throw new Error('This plugin expects the ios platform to exist.');
-  }
-
-  var iosFolder = context.opts.cordova.project ? context.opts.cordova.project.root : path.join(context.opts.projectRoot, 'platforms/ios/');
-  console_log("iosFolder: " + iosFolder);
-
-  fs.readdir(iosFolder, function (err, data) {
-    if (err) {
-      throw err;
+    if (context.opts.cordova.platforms.indexOf('ios') < 0) {
+        throw new Error('This plugin expects the ios platform to exist.');
     }
 
-    var projFolder;
-    var projName;
+    var iosFolder = context.opts.cordova.project ? context.opts.cordova.project.root : path.join(context.opts.projectRoot, 'platforms/ios/');
+    console_log("iosFolder: " + iosFolder);
 
-    // Find the project folder by looking for *.xcodeproj
-    if (data && data.length) {
-      data.forEach(function (folder) {
-        if (folder.match(/\.xcodeproj$/)) {
-          projFolder = path.join(iosFolder, folder);
-          projName = path.basename(folder, '.xcodeproj');
+    initLog(iosFolder);
+
+    fs.readdir(iosFolder, function (err, data) {
+        if (err) {
+            throw err;
         }
-      });
-    }
 
-    if (!projFolder || !projName) {
-      throw new Error("Could not find an .xcodeproj folder in: " + iosFolder);
-    }
+        var projFolder;
+        var projName;
 
-    var destFile = path.join(iosFolder, projName, 'Resources', projName + '.entitlements');
-    if (fs.existsSync(destFile)) {
-      console_log("File exists, not doing anything: " + destFile);
-    } else {
-      var sourceFile = path.join(context.opts.plugin.pluginInfo.dir, 'src/ios/resources/iCloud.entitlements');
-      fs.readFile(sourceFile, 'utf8', function (err, data) {
-        var resourcesFolderPath = path.join(iosFolder, projName, 'Resources');
-        fs.existsSync(resourcesFolderPath) || fs.mkdirSync(resourcesFolderPath);
-        fs.writeFileSync(destFile, data);
+        // Find the project folder by looking for *.xcodeproj
+        if (data && data.length) {
+            data.forEach(function (folder) {
+                if (folder.match(/\.xcodeproj$/)) {
+                    projFolder = path.join(iosFolder, folder);
+                    projName = path.basename(folder, '.xcodeproj');
+                }
+            });
+        }
 
-        var projectPath = path.join(projFolder, 'project.pbxproj');
+        if (!projFolder || !projName) {
+            throw new Error("Could not find an .xcodeproj folder in: " + iosFolder);
+        }
 
-        var pbxProject;
-        if (context.opts.cordova.project) {
-          pbxProject = context.opts.cordova.project.parseProjectFile(context.opts.projectRoot).xcode;
+        var destFile = path.join(iosFolder, projName, 'Resources', projName + '.entitlements');
+        if (fs.existsSync(destFile)) {
+            console_log("File exists, not doing anything: " + destFile);
         } else {
-          pbxProject = xcode.project(projectPath);
-          pbxProject.parseSync();
+            var sourceFile = path.join(context.opts.plugin.pluginInfo.dir, 'src/ios/resources/iCloud.entitlements');
+            fs.readFile(sourceFile, 'utf8', function (err, data) {
+                var resourcesFolderPath = path.join(iosFolder, projName, 'Resources');
+                fs.existsSync(resourcesFolderPath) || fs.mkdirSync(resourcesFolderPath);
+                fs.writeFileSync(destFile, data);
+
+                var projectPath = path.join(projFolder, 'project.pbxproj');
+
+                var pbxProject;
+                if (context.opts.cordova.project) {
+                    pbxProject = context.opts.cordova.project.parseProjectFile(context.opts.projectRoot).xcode;
+                } else {
+                    pbxProject = xcode.project(projectPath);
+                    pbxProject.parseSync();
+                }
+
+                pbxProject.addResourceFile(projName + ".entitlements");
+
+                var configGroups = pbxProject.hash.project.objects['XCBuildConfiguration'];
+                for (var key in configGroups) {
+                    var config = configGroups[key];
+                    if (config.buildSettings !== undefined) {
+                        config.buildSettings.CODE_SIGN_ENTITLEMENTS = '"' + projName + '/Resources/' + projName + '.entitlements"';
+                    }
+                }
+
+                // write the updated project file
+                fs.writeFileSync(projectPath, pbxProject.writeSync());
+                console_log("Added iCloud entitlements to project '" + projName + "'");
+
+                writeLog(iosFolder, projName);
+
+                deferral.resolve();
+            });
         }
+    });
 
-        pbxProject.addResourceFile(projName + ".entitlements");
-
-        var configGroups = pbxProject.hash.project.objects['XCBuildConfiguration'];
-        for (var key in configGroups) {
-          var config = configGroups[key];
-          if (config.buildSettings !== undefined) {
-            config.buildSettings.CODE_SIGN_ENTITLEMENTS = '"' + projName + '/Resources/' + projName + '.entitlements"';
-          }
-        }
-
-        // write the updated project file
-        fs.writeFileSync(projectPath, pbxProject.writeSync());
-        console_log("Added iCloud entitlements to project '" + projName + "'");
-
-        writeLog(iosFolder, projName);
-
-        deferral.resolve();
-      });
-    }
-  });
-
-  return deferral.promise;
+    return deferral.promise;
 };
